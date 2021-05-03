3f4bdd830dbcee1e41a9a49d61bff2683a9920b3
sabn730 <66404408+sabn730@users.noreply.github.com>
2021-05-03 21:35:35 +0500

    Delete Bulb directory

--- Bulb/app.js
+++
@@ -1,8 +0,0 @@
-function off() {
-    document.getElementById('on').className = "hidden"
-    document.getElementById('off').className = "show"
-}
-function on() {
-    document.getElementById('on').className = "show"
-    document.getElementById('off').className = "hidden"
-}
